c160acc9dbce4bc05398cec323c1c685a9996dda
juniorvilas <log_junior@hotmail.com>
2023-08-23 19:13:06 -0300

    refactor: change api

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['avatars.githubusercontent.com', 'localhost', 'aws-spacetime-bucket.s3.amazonaws.com'],
+      domains: ['avatars.githubusercontent.com', 'localhost', 'aws-spacetime-bucket.s3.amazonaws.com', 'pure-sands-43982-8154aa045897.herokuapp.com'],
     },
   }
   
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://pure-sands-43982-8154aa045897.herokuapp.com/',
 })
