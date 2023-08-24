6941488d8da1ab533384686b1ab1401a78dfa447
juniorvilas <log_junior@hotmail.com>
2023-08-24 13:43:19 -0300

    refactor: adjust layout to responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,14 +39,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        <main className="grid min-h-screen grid-cols-2">
+        <main className="grid min-h-screen lg:grid-cols-2">
           {/* Left */}
           <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
             {/* Blur */}
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
             {/* Stripes */}
-            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+            <div className="hidden lg:block absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
             {isAuthenticated ? <Profile /> : <SignIn />}
             <Hero />
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           </div>
 
           {/* Right */}
-          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
+          <div className="flex flex-col lg:flex-row max-h-screen  lg:overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             <Providers>{children}</Providers>
           </div>
         </main>
